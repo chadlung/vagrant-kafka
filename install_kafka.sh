78dd0fed20a47693b188d6c712ce73da7a21a00b
Chad Lung <chad.lung@gmail.com>
2015-03-11 20:59:58 -0500

    fix cd path

--- install_kafka.sh
+++ install_kafka.sh
@@ -4,7 +4,7 @@ echo "Downloading Kafka, please wait..."
 wget -q http://apache.mirrors.hoobly.com/kafka/0.8.2.1/kafka_2.10-0.8.2.1.tgz
 echo "Download complete, finishing install"
 tar -zxvf kafka_2.10-0.8.2.1.tgz
-cd kafka_kafka_2.10-0.8.2.1/
+cd kafka_2.10-0.8.2.1/
 echo "host.name=192.168.33.10" >> config/server.properties
 bin/zookeeper-server-start.sh config/zookeeper.properties &
 bin/kafka-server-start.sh config/server.properties &
